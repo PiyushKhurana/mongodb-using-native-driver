db7902ff85ef3c8eab57b356f1f568397a4aec36
PiyushKhurana <khuranapiyush27@gmail.com>
2019-08-14 18:24:44 +0530

    insertOne operation done

--- src/mongodb.js
+++ src/mongodb.js
@@ -16,6 +16,39 @@ MongoClient.connect(connectionUrl,{useNewUrlParser:true},(error,result)=>{
    
     const db = result.db(databaseName);
     
+    /**
+     * InsertOne
+     * Using Callback pattern 
+     */
+
+    db.collection('users').insertOne({
+        name:'test name 1',
+        age:'test age 1',
+        email:'Test1@test.com',
+        address:'test address 1'
+    },(error,result)=>{
+
+        if(error){
+            return console.log('Something went wrong while inserting !');
+        }
+
+        console.log(result.ops);
+        
+    });
+
+    /**
+     * InsertOne
+     * Using Promise 
+     */
+    
+    db.collection('users').insertOne({
+        name:'test name 2',
+        age:'test age 2',
+        email:'Test2@test.com',
+        address:'test address 2'
+    })
+    .then(result => console.log(result.ops))
+    .catch(error => console.log('Something went wrong while inserting !'));
     
 
 });
